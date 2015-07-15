0556f92be673866cf86e0bcfa6a2623049b949bb
Caique Moreira <caique.moreira@movile.com>
2015-07-15 14:37:54 -0300

    Initial Commit

---
+++ MovileUpTest/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  MovileUpTest
+//
+//  Created by Caique Moreira on 7/15/15
+//  Copyright (c) 2015 Movile. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
